1deb20b7fb62e953f2f32a175f44c5ab5ef596bb
alekseydmsoloviev <alekseydmsoloviev@gmail.com>
2025-06-05 07:17:36 +0300

    Add dedicated layout for settings fragment (#58)

--- app/src/main/java/com/example/serveradminapp/fragments/SettingsFragment.java
+++ app/src/main/java/com/example/serveradminapp/fragments/SettingsFragment.java
@@ -41,7 +41,7 @@ public class SettingsFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.activity_settings, container, false);
+        return inflater.inflate(R.layout.fragment_settings, container, false);
     }
 
     @Override
